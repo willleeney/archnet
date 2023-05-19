bdbd4a9d92e33203e70f9bed535a70b92f4d87a6
willleeney <will.leeney@outlook.com>
2023-05-19 16:24:47 +0100

    added optional arguments to the config

--- main.ts
+++ main.ts
@@ -9,18 +9,25 @@ import * as os from "os";
 import axios from "axios";
 
 type GPTArguments = {
-    seed: number; // RNG seed (default: -1)
-    threads: number; // number of threads to use during computation (default: 4)
-    n_predict: number; // number of tokens to predict (default: 128)
-    top_k: number; // top-k sampling (default: 40)
-    top_p: number; // top-p sampling (default: 0.9)
-    repeat_last_n: number; // last n tokens to consider for penalize (default: 64)
-    repeat_penalty: number; // penalize repeat sequence of tokens (default: 1.3)
-    ctx_size: number; // size of the prompt context (default: 2048)
-    temp: number; // temperature (default: 0.1)
-    batch_size: number; // batch size for prompt processing (default: 8)
-    model: string; // model path (default: gpt4all-lora-quantized.bin)
-};
+	seed?: number; // RNG seed (default: -1)
+    threads?: number; // number of threads to use during computation (default: 4)
+    n_predict?: number; // number of tokens to predict (default: 128)
+    top_k?: number; // top-k sampling (default: 40)
+    top_p?: number; // top-p sampling (default: 0.9)
+    repeat_last_n?: number; // last n tokens to consider for penalize (default: 64)
+    repeat_penalty?: number; // penalize repeat sequence of tokens (default: 1.3)
+    ctx_size?: number; // size of the prompt context (default: 2048)
+    temp?: number; // temperature (default: 0.1)
+    batch_size?: number; // batch size for prompt processing (default: 8)
+    model?: string; // model path (default: gpt4all-lora-quantized.bin)
+}
+
+ const ArchnetArguments: GPTArguments = {
+	seed: 42,
+	repeat_penalty: 0.0,
+	temp: 1.2,
+}
+
 
 export class GPT4All {
   private bot: ReturnType<typeof spawn> | null = null;
@@ -32,7 +39,7 @@ export class GPT4All {
   constructor(
     model: string = "gpt4all-lora-quantized",
     forceDownload: boolean = false,
-    decoderConfig: Partial<GPTArguments> = {},
+    decoderConfig: Partial<GPTArguments> = ArchnetArguments,
   ) {
     this.model = model;
     this.decoderConfig = decoderConfig;
